0282fbbda29fb9122e517e4567d337f77d44b5af
Javan <javan_dhein@outlook.com>
2023-11-06 14:22:21 -0300

    Remove unused code and import statements.

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,16 +1,5 @@
 'use server'
 
-import { z } from "zod";
-
-
-
-const schema = z.object({
-  name: z.string().min(3).max(50),
-  email: z.string().email(),
-  password: z.string().min(8).max(100),
-  confirmPassword: z.string().min(8).max(100),
-});
-
 export interface dataForm {
     name: string;
     email: string;
